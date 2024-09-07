5e39cc0e1229d39f7f5a3507ffb2868264e2fef6
Ujjwal <ukarora9@gmail.com>
2024-09-07 17:17:35 +0530

    Initial Commit

---
+++ GitSourceControl/GitSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitSourceControlApp.swift
+//  GitSourceControl
+//
+//  Created by Ujjwal Arora on 07/09/24.
+//
+
+import SwiftUI
+
+@main
+struct GitSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
